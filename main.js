aa4a15038d704d565ce095e26aa93a93ca72e8a7
Ash <ash@ashryanbeats.com>
2018-12-17 17:58:16 -0500

    Getting fieldsdepending on the artifact type.

--- main.js
+++ main.js
@@ -12,16 +12,47 @@ async function myPluginCommand(selection) {
   console.log("Plugin command is running!");
 
   const sharedArtifacts = cloud.getSharedArtifacts();
+  console.log(sharedArtifacts);
 
   const errorMsg = "You'll need to share a prototype or design spec first.";
-  let msgData = [];
+  let msgData = sharedArtifacts.map(artifact => {
+    let artifactDataHTML = `
+        <style>a {text-align: center;}</style>
+        <h2>Name: ${artifact.name}</h2>
+        <p>• Type: ${artifact.type}</p>
+        <p>• Access level: ${artifact.accessLevel}</p>
+        <p>• Comments allowed: ${artifact.allowComments ? "Yes" : "No"}</p>
+    `;
 
-  sharedArtifacts.map(artifact => {
-    msgData.push(`# Name: ${artifact.name}`);
-    msgData.push(`* Type: ${artifact.type}`);
-    msgData.push(`* Target Platform: ${artifact.targetPlatform}`);
-    msgData.push(`[View on the web](${artifact.url})`);
+    if (artifact.type === cloud.ArtifactType.PROTOTYPE) {
+      artifactDataHTML += `
+        <p>• Embed width and height: ${artifact.embedWidth} x ${
+        artifact.embedHeight
+      }</p>
+        <p>• Defaults to full page: ${
+          artifact.fullscreenInPage ? "Yes" : "No"
+        }</p>
+        <p>• Hotspot hints on: ${artifact.hotspotHints ? "Yes" : "No"}</p>
+        `;
+    } else if (artifact.type === cloud.ArtifactType.SPECS) {
+      artifactDataHTML += `
+        <p>• Target platform: ${artifact.targetPlatform}</p>
+        `;
+    }
+
+    artifactDataHTML += `
+      <a href=${artifact.url}>Click to view on xd.adobe.com</a>
+    `;
+
+    return artifactDataHTML;
   });
+
+  //   sharedArtifacts.map(artifact => {
+  //     msgData.push(`# Name: ${artifact.name}`);
+  //     msgData.push(`* Type: ${artifact.type}`);
+  //     msgData.push(`* Target Platform: ${artifact.targetPlatform}`);
+  //     msgData.push(`[View on the web](${artifact.url})`);
+  //   });
 
   const dialogContent = {
     title: "Shared Artifacts",
